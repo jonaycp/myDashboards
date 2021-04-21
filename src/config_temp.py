a70cb72d0f65c0b3d2305ff836a8ac820def0f3c
jonay <jonaycp@gmail.com>
2021-04-21 18:54:21 +0200

    First commits

---
+++ src/config_temp.py
@@ -0,0 +1,21 @@
+TWITTER_CONSUMER_KEY = ''
+TWITTER_CONSUMER_SECRET = ''
+TWITTER_ACCESS_TOKEN = ''
+TWITTER_ACCESS_TOKEN_SECRET = ''
+
+TWITTER_USERNAMES = [
+    'traderstewie',
+    'the_chart_life',
+    'canuck2usa',
+    'sunrisetrader',
+    'tmltrader'
+]
+
+DB_HOST = '127.0.0.1'
+DB_USER = 'postgres'
+DB_PASS = ''
+DB_NAME = 'etfdb'
+
+Clave_Api_Binance_secreta = ''
+Clave_Api_Binance_publica = ''
+
